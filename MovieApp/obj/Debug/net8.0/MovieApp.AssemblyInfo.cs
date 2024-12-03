f5d3a3cb2b9a260b9e00b122a0e5d0c95a5eccdc
SayaliMote <X00185002@mytudublin.ie>
2024-12-03 18:38:47 +0000

    sort

--- MovieApp/obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
+++ MovieApp/obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49808a87c94f32693de3fe3f9f8e5faa3ef0e7db")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
